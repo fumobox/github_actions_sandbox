e1ccd8a85883c0f411855e7fc4bb2291375310e0
fumobox <fumobox@users.noreply.github.com>
2022-05-19 12:28:03 +0000

    Deploying to gh-pages from @ fumobox/github_actions_sandbox@557c422d458ae08e76e60f4373310c61c8e320b4 🚀

---
+++ Android/Android_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,49 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Sandbox.SandboxController::StartButton()
+extern void SandboxController_StartButton_m71C71F031513ED69CCD2222C7F17D7D34FDEC7B3 (void);
+// 0x00000002 System.Void Sandbox.SandboxController::.ctor()
+extern void SandboxController__ctor_m2A38A3ED8868D0E374C91E2943176644EFEF1455 (void);
+static Il2CppMethodPointer s_methodPointers[2] = 
+{
+	SandboxController_StartButton_m71C71F031513ED69CCD2222C7F17D7D34FDEC7B3,
+	SandboxController__ctor_m2A38A3ED8868D0E374C91E2943176644EFEF1455,
+};
+static const int32_t s_InvokerIndices[2] = 
+{
+	3426,
+	3426,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	2,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
