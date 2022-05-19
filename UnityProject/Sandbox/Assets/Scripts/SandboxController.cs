4b4799864ffb41f1387ec447bf2fcf1d7e31e39f
fumobox <fumobox@gmail.com>
2022-05-19 15:41:58 +0900

    fix project

---
+++ UnityProject/Sandbox/Assets/Scripts/SandboxController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandboxController : MonoBehaviour
+{
+
+    public void StartButton()
+    {
+        Debug.Log("Start");
+    }
+
+}
+
